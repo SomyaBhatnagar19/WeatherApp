cbd93aa193162b4f5d20d8ec2afc154fba5e9ec2
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-08-22 18:17:09 +0530

    Making the decimal places go in temp.

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -6,14 +6,18 @@ import cloudIcon from "../Asset/cloud.png";
 import windIcon from "../Asset/wind.png";
 import humidityIcon from "../Asset/humidity.png";
 import noDataIcon from "../Asset/noData.png";
-
+import rainIcon from "../Asset/rain.png";
+import drizzleIcon from "../Asset/drizzle.png";
+import snowIcon from "../Asset/snow.png";
+import clearIcon from "../Asset/clear.png";
+import mistIcon from '../Asset/mist.png';
 const WeatherApp = () => {
   //adding the api to fetch the weather data
   const api_key = "291faa383342705eab54e84a99667849";
 
   const [cityInput, setCityInput] = useState(""); // State to track input
   const [weatherData, setWeatherData] = useState(null);
-
+  const [wicon, setwicon] = useState(cloudIcon);
   //creating a search function for our search bar/ button;
   const fetchWeatherData = async () => {
     if (cityInput === "") {
@@ -24,6 +28,40 @@ const WeatherApp = () => {
     const response = await fetch(url);
     const data = await response.json();
     setWeatherData(data);
+    // Update weather icon based on weather conditions
+    switch (data.weather[0].icon) {
+      case '01d':
+      case '01n':
+        setwicon(clearIcon);
+        break;
+      case '02d':
+      case '02n':
+      case '03d':
+      case '03n':
+        setwicon(cloudIcon);
+        break;
+      case '04d':
+      case '04n':
+        setwicon(drizzleIcon);
+        break;
+      case '09d':
+      case '09n':
+      case '10d':
+      case '10n':
+        setwicon(rainIcon);
+        break;
+      case '13d':
+      case '13n':
+        setwicon(snowIcon);
+        break;
+      case '50d':
+      case '50n':
+        setwicon(mistIcon); 
+        break;
+      default:
+        setwicon(clearIcon);
+        break;
+    }
   };
 
   const handleSearch = async () => {
@@ -53,9 +91,9 @@ const WeatherApp = () => {
       weatherData && (
         <>
           <div className="weather-image">
-            <img src={cloudIcon} alt="" />
+            <img src={wicon} alt="" />
           </div>
-          <div className="weather-temp">{weatherData.main.temp}°C</div>
+          <div className="weather-temp">{Math.floor(weatherData.main.temp)}°C</div>
           <div className="weather-location">{weatherData.name}</div>
           <div className="data-container">
             <div className="element">
